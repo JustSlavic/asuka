18d5360e286e54b14e107673d1ec486ced8c0466
radko <justslavic@gmail.com>
2021-08-10 18:34:14 +0300

    Change OS* macro to the COMPILER* macro
    Turns out that if you compile something with GCC, even on Windows,
    it does not contain Windows' types and intrinsics.
    For example, `__int32` should be `int` (probably have to fix it later
    for other platforms where int might be 16bit or something, although
    it's probably will be redundant).
    Another example is `__debugbreak` and `__builtin_trap`.

--- common/defines.hpp
+++ common/defines.hpp
@@ -1,8 +1,17 @@
 #ifndef ASUKA_COMMON_DEFINES_HPP
 #define ASUKA_COMMON_DEFINES_HPP
 
+#ifdef _MSC_VER
+#define ASUKA_COMPILER_MICROSOFT
+#elif __GNUC__
+#define ASUKA_COMPILER_GNU
+#elif __clang__
+#define ASUKA_COMPILER_CLANG
+#elif __MINGW32__
+#define ASUKA_COMPILER_MINGW
+#endif
 
-#ifdef ASUKA_OS_WINDOWS
+#ifdef ASUKA_COMPILER_MICROSOFT
 
 typedef __int8 int8;
 typedef __int16 int16;
@@ -14,17 +23,35 @@ typedef unsigned __int16 uint16;
 typedef unsigned __int32 uint32;
 typedef unsigned __int64 uint64;
 
+#define ASUKA_DEBUG_BREAK __debugbreak
+
+#endif // ASUKA_COMPILER_MICROSOFT
+
+#ifdef ASUKA_COMPILER_GNU
+
+typedef unsigned char      uint8;
+typedef unsigned short     uint16;
+typedef unsigned int       uint32;
+typedef unsigned long long uint64;
+
+typedef unsigned char      int8;
+typedef unsigned short     int16;
+typedef unsigned int       int32;
+typedef unsigned long long int64;
+
+#define ASUKA_DEBUG_BREAK __builtin_trap
+
+#endif // ASUKA_COMPILER_GNU_C
+
 #ifdef ASUKA_DEBUG
 #define ASSERT(COND) \
-    if (COND) {} else { __debugbreak(); } void(0)
+    if (COND) {} else { ASUKA_DEBUG_BREAK(); } void(0)
 #define ASSERT_MSG(COND, MSG) \
-    if (COND) {} else { __debugbreak(); } void(0)
-#else
+    if (COND) {} else { ASUKA_DEBUG_BREAK(); } void(0)
+#else // ASUKA_DEBUG
 #define ASSERT(COND)
 #define ASSERT_MSG(COND, MSG)
-#endif
-
-#endif // ASUKA_OS_WINDOWS
+#endif // ASUKA_DEBUG
 
 #ifdef ASUKA_OS_LINUX
 #endif // ASUKA_OS_LINUX
