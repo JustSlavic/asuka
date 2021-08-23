8296f8b17fec621750d7621b774eca84b052bb1a
radko <justslavic@gmail.com>
2021-08-23 17:20:39 +0300

    Update PNG loader

--- common/format_png.cpp
+++ common/format_png.cpp
@@ -11,11 +11,13 @@ uint32 change_endianess(uint32 n) {
          | ((n & 0x000000FF) << 24);
 }
 
+
 DECLARE_BIT_FIELD_T(PNG_ColorType, uint8,
     PNG_PALETTE_USED = 0x1,
     PNG_COLOR_USED = 0x2,
     PNG_ALPHA_CHANNEL_USED = 0x4
 );
+
 
 #pragma pack(push, 1)
 struct PNG_ChunkHeader {
@@ -23,9 +25,11 @@ struct PNG_ChunkHeader {
     uint8  type[4]; // type code
 };
 
+
 struct PNG_ChunkCRC {
     uint32 crc;
 };
+
 
 struct PNG_IHDRHeader {
     uint32 width;
@@ -38,6 +42,7 @@ struct PNG_IHDRHeader {
 };
 #pragma pack(pop)
 
+
 // Constants for rendering intent for sRGB chunk
 enum PNG_RenderingIntent : uint8 {
     PNG_PERCEPTUAL = 0,
@@ -46,11 +51,38 @@ enum PNG_RenderingIntent : uint8 {
     PNG_ABSOLUTE_COLORIMETRIC = 3,
 };
 
+
 // Available constants for interlace_method field in PNG_IHDRHeader struct
 enum PNG_InterlaceMethod : uint8 {
     PNG_INTERLACE_NONE = 0,
     PNG_INTERLACE_ADAM7 = 1,
 };
+
+
+struct bit_fetcher {
+    uint8* buffer;
+    uint32 bits;
+    uint32 available_bits;
+};
+
+
+uint32 get_bits(bit_fetcher* fetcher, int32 n) {
+    uint32 result;
+
+    if (fetcher->available_bits < n) {
+        do {
+            fetcher->bits |= ((uint32)(*fetcher->buffer++)) << fetcher->available_bits;
+            fetcher->available_bits += 8;
+        } while (fetcher->available_bits <= 24);
+    }
+
+    result = fetcher->bits & ((1 << n) - 1);
+    fetcher->bits = fetcher->bits >> n;
+    fetcher->available_bits -= n;
+
+    return result;
+}
+
 
 void* load_png_file(const char* filename) {
     auto result = os::load_entire_file(filename);
@@ -100,6 +132,7 @@ void* load_png_file(const char* filename) {
             header.filter_method = ihdr->filter_method;
             header.interlace_method = ihdr->interlace_method;
 
+            ASSERT(header.compression_method == 0); // At present, only compression method 0 (deflate/inflate compression with a sliding window of at most 32768 bytes) is defined. All standard PNG images must be compressed with this scheme.
             ASSERT(header.interlace_method == PNG_INTERLACE_NONE); // Adam7 interlace method is not supported.
 
             OutputDebugStringA(header.color_type.get(PNG_ColorType::PNG_ALPHA_CHANNEL_USED) ? "true" : "false");
@@ -136,7 +169,152 @@ void* load_png_file(const char* filename) {
                  chunk_header->type[2] == 'A' &&
                  chunk_header->type[3] == 'T')
         {
-            int x = 0;
+            uint8 CMF = *data;
+            uint8 compression_method = 0x0F & CMF;
+            ASSERT(compression_method == 8); // CM=8 denotes 'deflate' algorithm.
+
+            uint8 compression_info   = (0xF0 & CMF) >> 4;
+            // compression_info = (log_2(LZ77_window_size) - 8);
+            // => uint32 LZ77_window_size = 1 << (compression_info + 8);
+            // CINFO=7 indicates 32768 window size.
+            ASSERT(compression_info < 8); // CINFO above 7 are not allowed by the specification.
+            // compression_info is not defined if compression_method != 8
+
+            uint32 LZ77_window_size = 1 << (compression_info + 8);
+
+            uint8 FLG = *(data + 1);
+
+            uint8 FCHECK = (0x1F & FLG);
+            uint8 FDICT  = (0x20 & FLG) >> 5;
+            uint8 FLEVEL = (0xC0 & FLG) >> 6;
+
+            ASSERT((CMF * 256 + FLG) % 31 == 0); // The FCHECK value must be such that CMF and FLG, when viewed as
+                                                 // a 16-bit unsigned integer stored in MSB order (CMF*256 + FLG),
+                                                 // is a multiple of 31.
+
+            ASSERT(FDICT == 0);
+
+            uint8* zlib_data = data + 2;
+
+            bit_fetcher fetcher;
+            fetcher.buffer = zlib_data;
+            fetcher.bits = 0;
+            fetcher.available_bits = 0;
+
+            uint8 BFINAL;
+            uint8 BTYPE;
+            do {
+                BFINAL = get_bits(&fetcher, 1); // ((*zlib_data) & 0b1000'0000) >> 7;
+                BTYPE  = get_bits(&fetcher, 2); // ((*zlib_data) & 0b0110'0000) >> 5;
+
+                ASSERT(BTYPE != 3); // @debug: reserved value for error!
+
+                if (BTYPE == 0) { // Stored with no compression.
+                    zlib_data += 1; // Skip remaining bits in currently processed byte.
+
+                    uint16 LEN = *(uint16*)zlib_data;
+                    zlib_data += 2;
+
+                    uint16 NLEN = *(uint16*)zlib_data;
+                    zlib_data += 2;
+
+                    ASSERT(LEN == ~NLEN);
+                    // Test this on something.
+                } else {
+                    if (BTYPE == 3) {
+                        // @todo: return error;
+                        return nullptr;
+                    }
+
+                    if (BTYPE == 1) {
+                        // Compressed with fixed Huffman codes
+                    }
+
+                    if (BTYPE == 2) {
+                        // Compressed with dynamic Huffman codes
+                        // Computing Huffman codes
+                        uint32 HLIT  = get_bits(&fetcher, 5) + 257;
+                        uint32 HDIST = get_bits(&fetcher, 5) + 1;
+                        uint32 HCLEN = get_bits(&fetcher, 4) + 4;
+
+                        uint8 code_lengths_order[19] = { 16, 17, 18, 0, 8, 7, 9, 6, 10, 5, 11, 4, 12, 3, 13, 2, 14, 1, 15 };
+                        uint8 code_lengths[19] {};
+
+                        for (int i = 0; i < HCLEN; i++) {
+                            code_lengths[code_lengths_order[i]] = get_bits(&fetcher, 3);
+                        }
+
+                        // DEFLATE algorithm to generate codes
+                        // 1) Count the number of codes for each code length.
+                        //    Let bl_count[N] be the number of codes of length N, N >= 1.
+
+                        uint32 bl_count[19] {};
+
+                        for (int i = 0; i < 19; i++) {
+                            bl_count[code_lengths[i]]++;
+                        }
+
+                        // 2) Find the numerical value of the smallest code for each code length:
+
+                        int32 code = 0;
+                        bl_count[0] = 0;
+                        uint32 max_code;
+                        uint32 next_code[16] {};
+
+                        for (uint32 bits = 1; bits < 16; bits++) {
+                            code = (code + bl_count[bits-1]) << 1;
+                            next_code[bits] = code;
+                        }
+
+                        // 3) Assign numerical values to all codes, using consecutive
+                        //    values for all codes of the same length with the base
+                        //    values determined at step 2. Codes that are never used
+                        //    (which have a bit length of zero) must not be assigned a
+                        //    value.
+
+                        // for (uint32 n = 0;  n <= max_code; n++) {
+                        //     len = tree[n].Len;
+                        //     if (len != 0) {
+                        //         tree[n].Code = next_code[len];
+                        //         next_code[len]++;
+                        //     }
+                        // }
+
+                        // Apply Huffman codes
+                    }
+
+                    // Decode here
+                }
+            } while (!BFINAL);
+
+            uint8 debug_ = 0;
+
+            // do
+            //    read block header from input stream.
+            //    if stored with no compression
+            //       skip any remaining bits in current partially
+            //          processed byte
+            //       read LEN and NLEN (see next section)
+            //       copy LEN bytes of data to output
+            //    otherwise
+            //       if compressed with dynamic Huffman codes
+            //          read representation of code trees (see
+            //             subsection below)
+            //       loop (until end of block code recognized)
+            //          decode literal/length value from input stream
+            //          if value < 256
+            //             copy value (literal byte) to output stream
+            //          otherwise
+            //             if value = end of block (256)
+            //                break from loop
+            //             otherwise (value = 257..285)
+            //                decode distance from input stream
+
+            //                move backwards distance bytes in the output
+            //                stream, and copy length bytes from this
+            //                position to the output stream.
+            //       end loop
+            // while not last block
         }
         else if (chunk_header->type[0] == 'I' &&
                  chunk_header->type[1] == 'E' &&
