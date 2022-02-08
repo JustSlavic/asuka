c1319b086ff79a2830597c5f25dccbc6e4d0784f
radko <justslavic@gmail.com>
2022-02-09 01:10:29 +0300

    Add simple debugger prototype for linux
    That is in case I would want to develop it further and eventually
    turn into visual debugger for teaching memory for students.

--- build.sh
+++ build.sh
@@ -2,6 +2,11 @@
 
 [[ -d "build" ]] || mkdir build
 
-g++ src/linux_main.cpp -o build/main -g3 -DUNITY_BUILD -DASUKA_DEBUG -DASUKA_OS_LINUX=1 -DSOUND_ALSA=0 -Icommon -Isrc -lX11 -lasound
+# g++ src/linux_main.cpp -o build/main -g3 -DUNITY_BUILD -DASUKA_DEBUG -DASUKA_OS_LINUX=1 -DSOUND_ALSA=0 -Icommon -Isrc -lX11 -lasound
 # g++ web/linux_server.cpp -o build/server -DUNITY_BUILD -DASUKA_OS_LINUX -Icommon -Isrc
+
+
+gcc debugger/linux_debugger.cpp -o build/debugger -DUNITY_BUILD -DASUKA_OS_LINUX -Icommon -Isrc
+gcc debugger/debuggee.cpp -o build/debuggee -ggdb3 -static
+# gcc debugger/debuggee.s -o build/debuggee -ggdb3 -static -nostdlib
 
