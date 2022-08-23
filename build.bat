57ff9fca463eaf457077e235a11199e399f49829
radko <justslavic@gmail.com>
2022-08-23 22:53:37 +0300

    Remove non-existent compile targets

--- build.bat
+++ build.bat
@@ -22,11 +22,13 @@ SET COMMON_LIBS=User32.lib Xinput.lib Gdi32.lib Winmm.lib
 rem IF NOT EXIST build mkdir build
 REM pushd build
 
+
 REM ASUKA
 SET PDB_FILENAME=%date:~6,4%_%date:~3,2%_%date:~0,2%_%HOUR%_%time:~3,2%_%time:~6,2%_asuka.pdb
 echo WAITING FOR PDB > lock.tmp
 cl %COMMON_CL_FLAGS% %COMMON_FLAGS% %BUILD_SETTINGS% /DASUKA_DLL=1 /Feasuka ../src/asuka.cpp /LD /link /PDB:%PDB_FILENAME% %COMMON_LINKER_FLAGS%
 del lock.tmp
+
 
 REM Wenjie
 rem SET PDB_FILENAME=%date:~6,4%_%date:~3,2%_%date:~0,2%_%HOUR%_%time:~3,2%_%time:~6,2%_wenjie.pdb
@@ -34,8 +36,10 @@ rem echo WAITING FOR PDB > lock.tmp
 rem cl %COMMON_CL_FLAGS% %COMMON_FLAGS% /DASUKA_DLL=1 /Fmwenjie.map /Fewenjie ../wenjie/code/wenjie.cpp /LD /link /PDB:%PDB_FILENAME% %COMMON_LINKER_FLAGS%
 rem del lock.tmp
 
+
 REM Platform code
 cl %COMMON_CL_FLAGS% %COMMON_FLAGS% %BUILD_SETTINGS% /Femain ../src/win32_main.cpp /link %COMMON_LINKER_FLAGS% %COMMON_LIBS%
+
 
 REM Tests
 rem SET ASAN_TESTS=/fsanitize=address
@@ -47,19 +51,13 @@ rem cl %COMMON_CL_FLAGS% /DASUKA_DEBUG=1 /I../common /Fexaudio2_demo ../xaudio2/
 
 
 REM OpenGL
-cl %COMMON_CL_FLAGS% %COMMON_FLAGS% %WARNINGS% /I../common /Feopengl_demo ../opengl/opengl.cpp /link /PDB:opengl.pdb %COMMON_LINKER_FLAGS% %COMMON_LIBS% opengl32.lib
+rem cl %COMMON_CL_FLAGS% %COMMON_FLAGS% %WARNINGS% /I../common /Feopengl_demo ../opengl/opengl.cpp /link /PDB:opengl.pdb %COMMON_LINKER_FLAGS% %COMMON_LIBS% opengl32.lib
 
 REM D3D11
 cl %COMMON_CL_FLAGS% %COMMON_FLAGS% %WARNINGS% /I../common /Fed3d11_demo ../d3d11/d3d11.cpp /link /PDB:d3d11.pdb %COMMON_LINKER_FLAGS% %COMMON_LIBS% D3D11.lib D3DCompiler.lib
 
 REM D3D12
 rem cl %COMMON_CL_FLAGS% /DASUKA_DEBUG=1 /DUNITY_BUILD=1 /DASUKA_OS_WINDOWS=1 /I../common /Fed3d12_demo ../d3d12/d3d12.cpp /link /PDB:d3d12.pdb %COMMON_LINKER_FLAGS% %COMMON_LIBS% D3D12.lib DXGI.lib
-
-
-REM Malloc Visualizer
-rem cl /nologo /I../common /Fevisualizer ../malloc_proxy/visualizer.cpp /link /PDB:visualizer.pdb %COMMON_LINKER_FLAGS% User32.lib
-rem cl /nologo /I../common /Feproxy ../malloc_proxy/proxy.cpp /LD /link /PDB:proxy.pdb %COMMON_LINKER_FLAGS% User32.lib
-rem cl /nologo /MDd /DASUKA_DEBUG=1 /DUNITY_BUILD=1 /DASUKA_OS_WINDOWS=1 /I../common /Feclient_program ../malloc_proxy/client_program.cpp /link /PDB:client_program.pdb %COMMON_LINKER_FLAGS%
 
 
 REM NOCRT TEST BUILD
@@ -79,18 +77,9 @@ REM NEURAL NETWORKS
 rem cl %COMMON_CL_FLAGS% %COMMON_MY_FLAGS% /DASUKA_DEBUG=1 /I../common /Feneural ../neural/neural.cpp /link /PDB:neural.pdb %COMMON_LINKER_FLAGS% %COMMON_LIBS%
 
 
-REM EDITOR
-rem cl %COMMON_CL_FLAGS% /DASUKA_DEBUG=1 /I../common /Feeditor ../editor/editor.cpp /link /PDB:d3d11.pdb %COMMON_LINKER_FLAGS% %COMMON_LIBS%
-
-
 REM CLIENT-SERVER THINGS
 rem cl %COMMON_CL_FLAGS% %COMMON_MY_FLAGS% /Feclient  ../web/client.cpp          /link Ws2_32.lib
 rem cl %COMMON_CL_FLAGS% %COMMON_MY_FLAGS% /Feserver  ../web/server.cpp          /link Ws2_32.lib
-
-
-REM DEBUGGER THINGS
-rem cl %COMMON_CL_FLAGS% %COMMON_MY_FLAGS% /Fedebugger ../debugger/debugger.cpp /link /PDB:debugger.pdb
-rem cl %COMMON_CL_FLAGS% %COMMON_MY_FLAGS% /O2 /Fedebuggee ../debugger/debuggee.cpp /link /PDB:debuggee.pdb
 
 
 REM popd
